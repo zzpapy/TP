f8979b80f8f695e0f3a1a3e008fa17d956d94a41
zzpapy <gregory.pace@hotmail.fr>
2021-04-21 18:28:53 +0200

    search_pos_word

--- Sequence.php
+++ Sequence.php
@@ -8,14 +8,11 @@
             $j = 0;
             if($seq[$j] == $content[$i]){
                 while($j < count($seq)){
-                    // echo $content[$i] .PHP_EOL;
                     $pos = $i;
                     if($seq[$j] == $content[$i + 1]){
-                        // echo $content[$i + 1] .PHP_EOL;
                         $j++;
                         if($seq[$j] == $content[$i + 2]){
                             break;
-                            // echo $content[$i + 2] . " b " .PHP_EOL;
                         }
                     }
                     
@@ -24,6 +21,7 @@
             }
         }
         echo $pos;
+        
       
     };
     echo search_pos_word($content,  $seq);
